309ea9775b4b8b6d78226e98c762aba327108d95
Corey <corey@the-mannings.org>
2016-03-22 14:50:30 -0500

    exporter

---
+++ src/team/Exporter.java
@@ -0,0 +1,34 @@
+package team;
+
+public class Exporter {
+
+	private class GRun{
+		private int id;
+		private GRacer[] racers;
+		
+		public GRun(TRun run){
+			id = run.getID();
+			for(Racer r : run.getRacers()){
+				
+			}
+		}
+	}
+	
+	private class GRacer{
+		private int id;
+		private long start;
+		private long finish;
+		private boolean ended;
+		
+		public GRacer(TRacer racer){
+			id = racer.getID();
+			start = racer.getStartTime();
+			finish = racer.getFinishTime();
+			ended = racer.
+		}
+	}
+	
+	public void export(int run){
+		
+	}
+}
