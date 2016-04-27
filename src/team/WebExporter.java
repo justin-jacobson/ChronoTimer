03739004c9a39e5341905fd1fe2050bab569bfe8
dpp <dpp@uwm.edu>
2016-04-27 17:26:21 -0500

    WebExporter exports on separate thread.
    Otherwise chronotimer + gui waits until exporter finishes executing.

--- src/team/WebExporter.java
+++ src/team/WebExporter.java
@@ -19,83 +19,66 @@ public class WebExporter implements Exporter {
 	
 	@Override
 	public void export(Exportable e) {
-		try {
-			// build a string that contains JSON from console
-			String content = json.exportToString(e);
-			
-			if(content == null){
-				return;
-			}
-			
-			//Client will connect to this location
-			URL site = new URL("http://"+address+":"+port+"/sendresults");
-			HttpURLConnection conn = (HttpURLConnection) site.openConnection();
-
-			// now create a POST request
-			conn.setRequestMethod("POST");
-			conn.setDoOutput(true);
-			conn.setDoInput(true);
-			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
-			
-
-			// write out string to output buffer for message
-			out.writeBytes(content);
-			out.flush();
-			out.close();
-
-			InputStreamReader inputStr = new InputStreamReader(conn.getInputStream());
-
-			// string to hold the result of reading in the response
-			StringBuilder sb = new StringBuilder();
-
-			// read the characters from the request byte by byte and build up the sharedResponse
-			int nextChar = inputStr.read();
-			while (nextChar > -1) {
-				sb=sb.append((char)nextChar);
-				nextChar=inputStr.read();
-			}
-			System.out.println("Return String: "+ sb);
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-
+		Task t = new Task("sendresults",json.exportToString(e));
+		Thread newThread = new Thread(t);
+		newThread.start();
 	}
 	
-	public void reset(){
-		try {
-			//Client will connect to this location
-			URL site = new URL("http://"+address+":"+port+"/clear");
-			HttpURLConnection conn = (HttpURLConnection) site.openConnection();
-
-			// now create a POST request
-			conn.setRequestMethod("POST");
-			conn.setDoOutput(true);
-			conn.setDoInput(true);
-			DataOutputStream out = new DataOutputStream(conn.getOutputStream());
-			
-
-			// write out string to output buffer for message
-			out.writeBytes("");
-			out.flush();
-			out.close();
-
-			//InputStreamReader inputStr = new InputStreamReader(conn.getInputStream());
-
-//			// string to hold the result of reading in the response
-//			StringBuilder sb = new StringBuilder();
-//
-//			// read the characters from the request byte by byte and build up the sharedResponse
-//			int nextChar = inputStr.read();
-//			while (nextChar > -1) {
-//				sb=sb.append((char)nextChar);
-//				nextChar=inputStr.read();
-//			}
-//			System.out.println("Return String: "+ sb);
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
+	public void reset() {
+		Task t = new Task("clear","");
+		Thread newThread = new Thread(t);
+		newThread.start();
+	}
+	
+	private class Task implements Runnable {
+		
+		final String cmd;
+		final String txt;
+		
+		Task(String command, String message) {
+			cmd = command;
+			txt = message;
 		}
+
+		@Override
+		public void run() {
+			synchronized(WebExporter.this) {
+				try {
+					//Client will connect to this location
+					URL site = new URL("http://"+address+":"+port+"/" + cmd);
+					HttpURLConnection conn = (HttpURLConnection) site.openConnection();
+	
+					// now create a POST request
+					conn.setRequestMethod("POST");
+					conn.setDoOutput(true);
+					conn.setDoInput(true);
+					DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+					
+	
+					// write out string to output buffer for message
+					out.writeBytes(txt);
+					out.flush();
+					out.close();
+	
+					InputStreamReader inputStr = new InputStreamReader(conn.getInputStream());
+	
+					// string to hold the result of reading in the response
+					StringBuilder sb = new StringBuilder();
+	
+					// read the characters from the request byte by byte and build up the sharedResponse
+					int nextChar = inputStr.read();
+					while (nextChar > -1) {
+						sb=sb.append((char)nextChar);
+						nextChar=inputStr.read();
+					}
+					System.out.println("Return String: "+ sb);
+	
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		
 	}
 
 }
